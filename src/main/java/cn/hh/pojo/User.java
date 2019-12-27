08e4fcc82c862a95b15c5ee2777413e653262c0a
ZwxBuDai <1652221792@qq.com>
2019-12-27 17:37:22 +0800

    remove aaaaaa

--- src/main/java/cn/hh/pojo/User.java
+++ src/main/java/cn/hh/pojo/User.java
@@ -18,7 +18,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @NotNull(message = "用户名不能为空啊啊啊啊")
+    @NotNull(message = "用户名不能为空")
     private String username;
     private String password;
 }
